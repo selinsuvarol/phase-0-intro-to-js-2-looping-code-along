6c79d2cd56426d401004312b4ab1588c4fbec90b
Selin Su Varol <selinsuvarol@gmail.com>
2022-04-12 00:54:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+
+
+function writeCards(arr,event){
+    let cards = [];
+        for (let i=0;i<arr.length;i++){
+            const newNames=[];
+            cards.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+        }
+    return cards;
+}
+
+// console.log(writeCards(["Charlie", "Samip", "Ali"],"birtday"));
+
+function countDown(x){
+    while (x>0){
+        console.log(x);
+        x-=1;
+    }
+    console.log(x);
+}
+countDown(10);
+
